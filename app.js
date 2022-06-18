04bd6fe9573cf37e0a577c6c52414f8d9de2d989
pran_d <masterblaster50000@gmail.com>
2022-06-19 01:53:55 +0530

    all changes committed

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-// const feedRoutes = require('./routes/feed');
-
 const app = express();
 const mongoose = require("mongoose");
+require('dotenv').config();
 
+const dbURL = process.env.dbURL;
 
 app.use(bodyParser.json());
 const postsRoute = require("./routes/posts");
-
-// app.use(bodyParser.json());
-
 // app.use((req, res, next) => { //to get access to the server from postman
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
@@ -19,37 +15,23 @@ const postsRoute = require("./routes/posts");
 //     next();
 // });
 
-// app.use('/feed', feedRoutes); 
-
-// try{
-//     app.listen(3000)
-//     console.log("listening to port 3000");
-// }catch(err){
-//     console.log(err);
-// }
-
-app.listen(3000);
-
-// app.use("/bdays", ()=>{
-//     console.log("Middleware is running");
-// });
-
 app.use("/posts", postsRoute);
-
 app.get("/", (req, res)=>{
     res.send("Hello Home Route")
 });
 
-// app.get("/bdays", (req, res)=>{
-//     res.send("This is Bdays route")
-// });
-
-mongoose.connect(
-    "mongodb+srv://pran:dontlookdown@nodetuts.jmktc.mongodb.net/nodetuts?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    () => {
-        console.log("Connected to Database");
-    }
-);
+try{
+    mongoose.connect(
+        dbURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
+        () => {
+            console.log("Connected to Database");
+            app.listen(3000);
+            console.log("Listening to port 3000");
+        }
+    )
+}catch(err){
+    console.log(err);
+}
